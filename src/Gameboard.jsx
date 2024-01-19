14b51ecdc42d35e0d080e539d9f2ebaf3b22860e
kevinalappat09 <35099148+kevinalappat09@users.noreply.github.com>
2024-01-19 16:35:15 +0530

    Made the correct game :)

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -4,22 +4,33 @@ function Gameboard() {
     const [gameOver, setGameOver] = useState(false);
     const [score, setScore] = useState(0);
     const [currentCards, setCurrentCards] = useState([]);
-    const [activeId, setActiveId] = useState(0);
+    const [activeId, setActiveId] = useState([]);
 
     const setNewActiveCard = (e) => {
         const id = Number(e.currentTarget.getAttribute("data-id"));
-        if(id === activeId) {
+        if(activeId.includes(id)) {
             setGameOver(true);
         } else {
-            setActiveId(id);
+            const newActiveIds = [];
+            const newRandom1 = Math.floor(Math.random() * 2);
+            newActiveIds.push(currentCards[newRandom1].id);
+
+            let newRandom2 = Math.floor(Math.random() * 2);
+            while(newRandom2 === newRandom1) {
+                newRandom2 = Math.floor(Math.random() * 2);
+            }
+            newActiveIds.push(currentCards[newRandom2].id);
+            
+            setActiveId(newActiveIds);
             setScore(score+1);
         }
+        
     }
 
     useEffect(() => {
         let isMounted = true;
 
-        if (activeId === 0) {
+        if (activeId.length === 0) {
             const getThreeRandomPokemon = async () => {
                 const getRandomID = () => Math.floor(Math.random() * 500 + 1);
 
@@ -59,24 +70,23 @@ function Gameboard() {
 
             getThreeRandomPokemon();
         } else {
-            const getTwoRandomPokemon = async () => {
+            const getRandomPokemon = async () => {
                 const randomID1 = Math.floor(Math.random() * 500 + 1);
-                const randomID2 = Math.floor(Math.random() * 500 + 1);
 
-                const lastActiveResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${activeId}/`);
-                const lastActiveData = await lastActiveResponse.json();
+                const lastActivePokemons = [];
+                for(let i=0;i<activeId.length; i++) {
+                    const lastActiveResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${activeId[i]}/`);
+                    const lastActiveData = await lastActiveResponse.json();
+                    const lastActiveObject = {
+                        id: lastActiveData.id,
+                        name : lastActiveData.name,
+                        imageURL : lastActiveData.sprites.front_default,
+                    }
+                    lastActivePokemons.push(lastActiveObject);
+                }
 
                 const response1 = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomID1}/`);
                 const data1 = await response1.json();
-
-                const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomID2}/`);
-                const data2 = await response2.json();
-
-                const lastActiveObject = {
-                    id: lastActiveData.id,
-                    name : lastActiveData.name,
-                    imageURL : lastActiveData.sprites.front_default,
-                }
 
                 const PokemonObject1 = {
                     id: data1.id,
@@ -84,23 +94,16 @@ function Gameboard() {
                     imageURL: data1.sprites.front_default,
                 }
 
-                const PokemonObject2 = {
-                    id: data2.id,
-                    name: data2.name,
-                    imageURL: data2.sprites.front_default,
-                }
-
-
                 const activeCardPosition = Math.floor(Math.random() * 2);
-                const newCards = [PokemonObject1, PokemonObject2];
-                newCards.splice(activeCardPosition,0,lastActiveObject);
+                const newCards = [lastActivePokemons[0], lastActivePokemons[1]];
+                newCards.splice(activeCardPosition,0,PokemonObject1);
 
                 if (isMounted) {
                     setCurrentCards(newCards);
                 }
             }
 
-            getTwoRandomPokemon();
+            getRandomPokemon();
         }
 
         return () => {
